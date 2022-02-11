ffe0dc477780033d545d6257e928b1bd8eb0200b
Fabian5150 <fabian.strauch.04@web.de>
2022-02-11 13:56:07 +0100

    implementation of modal

--- client/src/components/Statistics.js
+++ client/src/components/Statistics.js
@@ -1,8 +1,20 @@
 //packages
 import React from "react";
+//components
+import Modal from "./Modal";
 
 const Statistics = () => {
-    return <div>Statistiken</div>
+    return (
+        <div>
+            Statistiken
+            <Modal 
+                title="Titel"
+                content={"hui"}
+                actions={"yae"}
+                onDismiss={() => console.log(":(")}
+            />
+        </div>
+    )
 }
 
 export default Statistics;
