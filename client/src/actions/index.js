728950be1df51f5f2a911027a1764e01c1a45493
Fabian5150 <fabian.strauch.04@web.de>
2022-02-28 16:54:07 +0100

    posting to backend api kinda works, just not w redux somehow

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,18 @@
+//api connection
+import trees from '../apis/trees'
 //action types
 import {
   CREATE_TREE
 } from './types'
 
-export const createTree = formValues => {
+/* export const createTree = formValues => {
   return {
     type: CREATE_TREE,
     payload: formValues
   }
+} */
+
+export const createTree = formValues => async dispatch => {
+  const res = trees.post('/treePreviews', formValues)
+  dispatch({ type: CREATE_TREE, payload: res.data })
 }
--- client/src/components/trees/TreeCreate.js
+++ client/src/components/trees/TreeCreate.js
@@ -1,13 +1,21 @@
 //packages
 import React from "react";
+import { useDispatch } from 'react-redux'
 //components
 import Modal from "../Modal";
 import TreeForm from "./TreeForm";
 import { createTree } from "../../actions";
+//api connection
+import trees from '../../apis/trees'
 
 const TreeCreate = ({ show, setShow }) => {
+  const dispatch = useDispatch()
+
   const onSubmit = formValues => {
     console.log(formValues)
+    trees.post('/treePreviews', formValues)
+    //createTree(formValues)
+    //dispatch({ type: "CREATE_TREE" })
   }
 
   const modalContent = () => {
