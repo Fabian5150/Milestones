54a5546adb6e1c283cc69b3c54de24c9df757de0
Fabian5150 <fabian.strauch.04@web.de>
2022-03-01 20:08:21 +0100

    hardcoded tree in Tree component

--- client/src/components/trees/Tree.js
+++ client/src/components/trees/Tree.js
@@ -1,13 +1,87 @@
-//packages
 import React from "react";
+import Tree from 'react-d3-tree';
 
-const Tree = ({match:{params}}) => {
+// This is a simplified example of an org chart with a depth of 2.
+// Note how deeper levels are defined recursively via the `children` property.
+const orgChart = {
+  name: 'CEO',
+  children: [
+    {
+      name: 'Manager',
+      attributes: {
+        done: true,
+        department: 'Production',
+      },
+      children: [
+        {
+          name: 'Foreman',
+          attributes: {
+            done: true,
+            department: 'Fabrication',
+          },
+          children: [
+            {
+              name: 'Worker',
+              attributes: {
+                done: false
+              }
+            },
+          ],
+        },
+        {
+          name: 'Foreman',
+          attributes: {
+            department: 'Assembly',
+            done: false
+          },
+          children: [
+            {
+              name: 'Worker',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+const App = () => {
+  const renderRectSvgNode = ({ nodeDatum, toggleNode }) => (
+    <g>
+      <circle 
+        r={20} 
+        onClick={() => console.log(nodeDatum)} 
+        fill={`${nodeDatum.attributes?.done ? "green" : "red"}`} 
+      />
+      <foreignObject x="-25" y="0" width="50" height="50">
+        <button className="mini ui circular icon button" onClick={() => console.log("Holá!")}>
+          <i className="add icon"></i>
+        </button>
+      </foreignObject>
+      
+      <text fill={`${nodeDatum.name === "CEO" ? "red" : "black"}`} stroke={`${nodeDatum.name === "CEO" ? "red" : "black"}`} strokeWidth="0.3" x="25">
+        {nodeDatum.name}
+      </text>
+      {nodeDatum.attributes?.department && (
+        <text fill="black" x="25" y="25" strokeWidth="0.3">
+          Department: {nodeDatum.attributes?.department}
+        </text>
+      )}
+    </g>
+  );
+  
   return (
-    <div>
-      <h1><i className="sitemap icon"/> Tree</h1> 
-      {`Tree with id ${params.id}`}
+    <div id="treeWrapper" style={{ width: "100vw", height: "100vh" }}>
+      <Tree 
+        data={orgChart} 
+        collapsible={false} 
+        zoomable={false}
+        orientation='vertical' 
+        translate={{ x: window.innerWidth / 2, y: window.innerHeight / 2 }}
+        renderCustomNodeElement={renderRectSvgNode}          
+        />
     </div>
-  )
+  );
 }
 
-export default Tree;
+export default App;
