58094ec9594a1206b6c1017cee92f6e6951158cf
Fabian5150 <fabian.strauch.04@web.de>
2022-03-06 23:15:06 +0100

    render tree's category's icon in list item

--- client/src/components/trees/TreeList.js
+++ client/src/components/trees/TreeList.js
@@ -1,20 +1,73 @@
 //packages
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 //functions
-import { fetchTreePreviews } from "../../actions";
+import { fetchTreePreviews, fetchCategories } from "../../actions";
 
 const TreeList = ( {match: {params}} ) => {
   const [treePreviews, setTreePreviews] = useState()
+  const [categories, setCategories] = useState()
 
   useEffect(() => {
     fetchTreePreviews()
     .then(({ payload }) => {
       setTreePreviews(payload)
     })
+
+    fetchCategories()
+    .then(({ payload }) => {
+      setCategories(payload)
+    })
   }, [])
 
+  const categoryIcon = treePreview => {
+    let icon = "sitemap"
+
+    if(categories) {
+      categories.forEach(category => {
+        if(category.value === treePreview.category){
+          icon = category.icon
+        }
+      })
+    } 
+
+    return icon
+  }
+
+  const renderList = () => {
+    if(!treePreviews){
+      return <div>Loading...</div>
+    }
+
+    return treePreviews.map(preview => {
+      return (
+        <div className="item" key={preview.id}>
+          <Link to={`/tree/${preview.id}`} className="right floated ui icon button">
+            <i className="external icon"></i>
+          </Link>
+          <i className={`${categoryIcon(preview)} large middle aligned icon`}/>
+          <div className="content">
+              {preview.title} ({preview.category})
+              <div className="description">
+                {preview.description}
+              </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   if(params.key === "all"){
-    return <div>Alle Bäume</div>
+    return (
+      <>
+        <h1>Alle Bäume</h1>
+        <div className="ui green segment">
+          <div className="ui celled list">
+            {renderList()}
+          </div>
+        </div>        
+      </>
+    )
   } else {
     return (
       <div>
