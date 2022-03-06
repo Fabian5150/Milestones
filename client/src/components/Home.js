73407e207a20d279e1e7c164e1b3d22994dff2a9
Fabian5150 <fabian.strauch.04@web.de>
2022-03-06 22:20:44 +0100

    Link in home first 4 trees segment to list of all trees

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,12 @@
 //packages
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import _ from "lodash";
 //functions
 import { fetchTreePreviews } from "../actions";
 //components
 import TreeCreate from "./trees/TreeCreate";
 import TreePreview from "./trees/TreePreview";
-
-const myTree = {
-  title: "Mein wirklich Erster",
-  description: "Mein erster Baum, ich bin so aufgeregt :o",
-  category: "Musik",
-  id: 1
-}
 
 const Home = () => {
   const [show, setShow] = useState(false)
@@ -49,13 +43,13 @@ const Home = () => {
         <i className="plus icon"/> Neuen Baum erstellen 
       </button>
 
-      <h1>Willkomen zurück!</h1>
+      <h1>Willkommen zurück!</h1>
       <div className="ui green segment">
         <h3 className="ui left floated header">Daran haben sie zuletzt gearbeitet:</h3>
-        <button className="right floated tiny ui right labeled icon button">
+        <Link to="/search/all/latestFirst" className="right floated tiny ui right labeled icon button">
           <i className="right arrow icon" />
           Zeige alle Bäume
-        </button>
+        </Link>
 
         <div className="ui clearing hidden divider" />
 
--- client/src/components/trees/TreeList.js
+++ client/src/components/trees/TreeList.js
@@ -1,13 +1,28 @@
 //packages
-import React from "react";
+import React, { useState, useEffect } from "react";
+//functions
+import { fetchTreePreviews } from "../../actions";
 
 const TreeList = ( {match: {params}} ) => {
-  return (
-    <div>
-      <h1>TreeList</h1>
-      <p>{`Trees filterd by ${params.searchBy}, with the key of ${params.key}.`}</p>
-    </div>
-  )
+  const [treePreviews, setTreePreviews] = useState()
+
+  useEffect(() => {
+    fetchTreePreviews()
+    .then(({ payload }) => {
+      setTreePreviews(payload)
+    })
+  }, [])
+
+  if(params.searchBy === "all"){
+    return <div>Alle Bäume</div>
+  } else {
+    return (
+      <div>
+        <h1>TreeList</h1>
+        <p>{`Trees filterd by ${params.searchBy}, with the key of ${params.key}.`}</p>
+      </div>
+    )
+  }
 }
 
 export default TreeList;
